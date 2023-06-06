3a97c7a3a46c898e1f8ef9fdf7c1768d48a2102d
MarcianoJuly <marcianocruzeiri@gmail.com>
2023-06-06 15:59:47 -0300

    quase fim

--- src/main/java/com/loucura/aaaaaaaa/controller/ListController.java
+++ src/main/java/com/loucura/aaaaaaaa/controller/ListController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -67,5 +68,16 @@ public class ListController {
     @CrossOrigin(origins = "http://localhost:4200")
     public DataClient create(@RequestBody DataClient data){
         return dataClientRepositorio.save(data); 
+    }
+
+    @PutMapping("/edit/{CPF}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<DataClient> put(@PathVariable String CPF, @RequestBody DataClient dados) {
+        return dataClientRepositorio.findById(CPF)
+        .map(recordFound -> {recordFound = dados;
+                            DataClient update = dataClientRepositorio.save(recordFound);
+                            return ResponseEntity.ok().body(update);
+                        })
+        .orElse(ResponseEntity.notFound().build());
     }
 }
